5ed095d34d5af31808a0889b7024bce00465418c
Ismael Reyes <ismael.kix2902@gmail.com>
2021-01-03 21:38:40 +0100

    Opened Qr screen when clicked button

--- app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
@@ -1,18 +1,22 @@
 package es.kix2902.cd2spotify.ui.main
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import es.kix2902.cd2spotify.databinding.ActivityMainBinding
+import es.kix2902.cd2spotify.ui.qr.QrActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.readBarcode.setOnClickListener {
+            startActivity(Intent(this, QrActivity::class.java))
+        }
     }
 }
