a721a0a0b8a7621f76f409d6d72f22a2890b3e41
Ismael Reyes <ismael.kix2902@gmail.com>
2021-01-05 18:57:07 +0100

    Created error screen

---
+++ app/src/main/java/es/kix2902/cd2spotify/ui/error/ErrorActivity.kt
@@ -0,0 +1,32 @@
+package es.kix2902.cd2spotify.ui.error
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.kix2902.cd2spotify.R
+import es.kix2902.cd2spotify.databinding.ActivityErrorBinding
+
+class ErrorActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_ERROR = "error"
+        private const val ERROR_INVALID = -1
+        const val ERROR_PERMISSION = 0
+        const val ERROR_INITIALIZATION = 1
+        const val ERROR_READING = 2
+    }
+
+    private lateinit var binding: ActivityErrorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityErrorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        when (intent.getIntExtra(EXTRA_ERROR, ERROR_INVALID)) {
+            ERROR_PERMISSION -> binding.errorText.setText(R.string.error_permission)
+            ERROR_INITIALIZATION -> binding.errorText.setText(R.string.error_initialization)
+            ERROR_READING -> binding.errorText.setText(R.string.error_reading)
+            ERROR_INVALID -> finish()
+        }
+    }
+}
