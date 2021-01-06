91dda3bc451c29cf2cf13506570141e178643684
Ismael Reyes <ismael.kix2902@gmail.com>
2021-01-06 21:01:51 +0100

    Added theme selector in main screen

--- app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package es.kix2902.cd2spotify.ui.main
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import es.kix2902.cd2spotify.BuildConfig
+import es.kix2902.cd2spotify.R
 import es.kix2902.cd2spotify.databinding.ActivityMainBinding
 import es.kix2902.cd2spotify.ui.qr.QrActivity
 
@@ -20,6 +25,27 @@ class MainActivity : AppCompatActivity() {
         }
         binding.showHistory.setOnClickListener {
 
+        }
+        binding.changeTheme.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.theme_dialog_title)
+                .setItems(R.array.theme_entries) { dialog, which ->
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        when (which) {
+                            0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                            2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                            3 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        }
+                    } else {
+                        when (which) {
+                            0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                            2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        }
+                    }
+                }
+                .show()
         }
     }
 }
