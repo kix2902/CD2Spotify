f624bef9f5abbf855d5542e0d5a23d7fa4f1f005
Ismael Reyes <ismael.kix2902@gmail.com>
2021-01-09 21:14:04 +0100

    Created model for release info

---
+++ app/src/main/java/es/kix2902/cd2spotify/data/models/Release.kt
@@ -0,0 +1,11 @@
+package es.kix2902.cd2spotify.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "releases")
+data class Release(
+    @PrimaryKey val barcode: String,
+    val title: String,
+    val author: String
+)
