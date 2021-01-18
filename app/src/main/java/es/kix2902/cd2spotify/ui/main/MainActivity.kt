72cb8a0e4481701266f3eb711d913c4e63e8abc6
Ismael Reyes <ismael.kix2902@gmail.com>
2021-01-18 22:53:03 +0100

    Updated main screen to use app-auth with spotify login process

--- app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/main/MainActivity.kt
@@ -8,26 +8,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.isVisible
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.spotify.sdk.android.auth.AuthorizationClient
-import com.spotify.sdk.android.auth.AuthorizationRequest
-import com.spotify.sdk.android.auth.AuthorizationResponse
 import es.kix2902.cd2spotify.R
 import es.kix2902.cd2spotify.databinding.ActivityMainBinding
-import es.kix2902.cd2spotify.ui.error.ErrorActivity
 import es.kix2902.cd2spotify.ui.qr.QrActivity
+import net.openid.appauth.AuthorizationService
 
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private const val SPOTIFY_CLIENT_ID = "5bf1991a3f384cfa8c330b7174cb703a"
-        private const val SPOTIFY_REDIRECT_URL = "cd2spotify://callback"
         private const val SPOTIFY_AUTH_REQUEST_CODE = 2345
     }
 
     private lateinit var binding: ActivityMainBinding
 
     private val viewModel: MainViewModel by viewModels()
+
+    private val authService: AuthorizationService by lazy { AuthorizationService(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,25 +42,8 @@ class MainActivity : AppCompatActivity() {
         })
 
         binding.authorize.setOnClickListener {
-            val request = AuthorizationRequest
-                .Builder(
-                    SPOTIFY_CLIENT_ID,
-                    AuthorizationResponse.Type.TOKEN,
-                    SPOTIFY_REDIRECT_URL
-                )
-                .setScopes(
-                    arrayOf(
-                        "playlist-modify-public",
-                        "user-follow-modify",
-                        "user-follow-read",
-                        "user-library-modify",
-                        "user-read-private",
-                        "user-library-read"
-                    )
-                )
-                .setShowDialog(false)
-                .build()
-            AuthorizationClient.openLoginActivity(this, SPOTIFY_AUTH_REQUEST_CODE, request)
+            val authIntent = authService.getAuthorizationRequestIntent(viewModel.authRequest)
+            startActivityForResult(authIntent, SPOTIFY_AUTH_REQUEST_CODE)
         }
         binding.readBarcode.setOnClickListener {
             startActivity(Intent(this, QrActivity::class.java))
@@ -104,19 +84,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == SPOTIFY_AUTH_REQUEST_CODE) {
-            val response = AuthorizationClient.getResponse(resultCode, data)
-            when (response.type) {
-                AuthorizationResponse.Type.TOKEN -> {
-                    viewModel.saveSpotifyToken(response.accessToken)
-                }
-                AuthorizationResponse.Type.ERROR -> {
-                    val intent = Intent(this, ErrorActivity::class.java)
-                    intent.putExtra(ErrorActivity.EXTRA_ERROR, ErrorActivity.ERROR_SPOTIFY_AUTH)
-                    startActivity(intent)
-                }
-                else -> {
-                }
-            }
+            viewModel.requestAccessToken(data!!, authService)
         }
     }
 }
--- app/src/main/java/es/kix2902/cd2spotify/ui/main/MainViewModel.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/main/MainViewModel.kt
@@ -1,16 +1,24 @@
 package es.kix2902.cd2spotify.ui.main
 
 import android.app.Application
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import es.kix2902.cd2spotify.data.PreferencesRepository
 import es.kix2902.cd2spotify.domain.IsSpotifyInitialized
-import es.kix2902.cd2spotify.domain.SaveSpotifyToken
+import es.kix2902.cd2spotify.domain.SaveSpotifyAuth
 import es.kix2902.cd2spotify.domain.UseCase
+import net.openid.appauth.*
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    companion object {
+        private const val SPOTIFY_CLIENT_ID = "5bf1991a3f384cfa8c330b7174cb703a"
+        private const val SPOTIFY_REDIRECT_URL = "cd2spotify://callback"
+    }
 
     private val _spotifyInit = MutableLiveData(false)
     val spotifyInit: LiveData<Boolean> = _spotifyInit
@@ -19,7 +27,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         PreferencesRepository.getInstance(application.applicationContext)
 
     private val isSpotifyInitialized = IsSpotifyInitialized(viewModelScope, preferencesRepository)
-    private val saveSpotifyToken = SaveSpotifyToken(viewModelScope, preferencesRepository)
+    private val saveSpotifyToken = SaveSpotifyAuth(viewModelScope, preferencesRepository)
 
     fun checkSpotify() {
         isSpotifyInitialized.invoke(UseCase.None()) {
@@ -28,9 +36,44 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    fun saveSpotifyToken(token: String) {
-        saveSpotifyToken.invoke(token) {
-            _spotifyInit.value = true
+    private val serviceConfig = AuthorizationServiceConfiguration(
+        Uri.parse("https://accounts.spotify.com/authorize"),
+        Uri.parse("https://accounts.spotify.com/api/token")
+    )
+    private val authState = AuthState(serviceConfig)
+    val authRequest = AuthorizationRequest.Builder(
+        serviceConfig,
+        SPOTIFY_CLIENT_ID,
+        ResponseTypeValues.CODE,
+        Uri.parse(SPOTIFY_REDIRECT_URL)
+    ).setScopes(
+        "playlist-modify-public",
+        "user-follow-modify",
+        "user-follow-read",
+        "user-library-modify",
+        "user-read-private",
+        "user-library-read"
+    ).build()
+
+    fun requestAccessToken(data: Intent, authService: AuthorizationService) {
+        val resp = AuthorizationResponse.fromIntent(data)
+        val ex = AuthorizationException.fromIntent(data)
+        authState.update(resp, ex)
+
+        if (resp != null) {
+            authService.performTokenRequest(resp.createTokenExchangeRequest()) { newResp, newEx ->
+                authState.update(newResp, newEx)
+                if (newResp != null) {
+                    saveSpotifyToken.invoke(authState) {
+                        _spotifyInit.value = true
+                    }
+                } else {
+                    _spotifyInit.value = false
+                }
+            }
+
+        } else {
+            _spotifyInit.value = false
         }
     }
 }
