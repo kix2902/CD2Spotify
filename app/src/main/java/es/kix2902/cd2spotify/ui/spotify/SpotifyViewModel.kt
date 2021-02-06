706ce9bbf69a94e265b78697be8aa458a20f87b4
Ismael Reyes <ismael.kix2902@gmail.com>
2021-02-06 19:14:41 +0100

    Updated spotify viewmodel with process to request album info after finding release

--- app/src/main/java/es/kix2902/cd2spotify/ui/spotify/SpotifyViewModel.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/spotify/SpotifyViewModel.kt
@@ -7,19 +7,25 @@ import androidx.lifecycle.viewModelScope
 import es.kix2902.cd2spotify.data.DatabaseRepository
 import es.kix2902.cd2spotify.data.NetworkRepository
 import es.kix2902.cd2spotify.data.models.Release
-import es.kix2902.cd2spotify.domain.GetRelease
+import es.kix2902.cd2spotify.domain.FindRelease
+import es.kix2902.cd2spotify.domain.SearchAlbum
 import es.kix2902.cd2spotify.domain.UseCase
 
 class SpotifyViewModel(application: Application) : AndroidViewModel(application) {
 
     private val dbRepository = DatabaseRepository.getInstance(application.applicationContext)
-    private val getRelease = GetRelease(viewModelScope, dbRepository, NetworkRepository)
+    private val networkRepository = NetworkRepository.getInstance(application.applicationContext)
+
+    private val getRelease = FindRelease(viewModelScope, dbRepository, networkRepository)
+    private val searchAlbum = SearchAlbum(viewModelScope, networkRepository)
 
     fun findBarcode(barcode: String) {
         getRelease.invoke(barcode) {
             when (it) {
                 is UseCase.Result.Success<*> -> {
                     val release = it.data as Release
+                    searchSpotify(release)
+
                     Toast.makeText(
                         getApplication(),
                         "${release.author} - ${release.title}",
@@ -30,6 +36,11 @@ class SpotifyViewModel(application: Application) : AndroidViewModel(application)
                     Toast.makeText(getApplication(), "Error", Toast.LENGTH_LONG).show()
                 }
             }
+        }
+    }
+
+    private fun searchSpotify(release: Release) {
+        searchAlbum.invoke(release) {
         }
     }
 }
