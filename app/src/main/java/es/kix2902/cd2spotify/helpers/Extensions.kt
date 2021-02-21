427dc2d7d7664af87b1424d9a66870e14e5d8202
Ismael Reyes <ismael.kix2902@gmail.com>
2021-02-21 21:31:04 +0100

    Removed unused extension

--- app/src/main/java/es/kix2902/cd2spotify/helpers/Extensions.kt
+++ app/src/main/java/es/kix2902/cd2spotify/helpers/Extensions.kt
@@ -1,15 +1,10 @@
 package es.kix2902.cd2spotify.helpers
 
 import androidx.recyclerview.widget.RecyclerView
-import es.kix2902.cd2spotify.data.models.Musicbrainz
 
 fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
     itemView.setOnClickListener {
         event.invoke(adapterPosition, itemViewType)
     }
     return this
-}
-
-fun Musicbrainz.ReleaseWithArtists.hasInfo(): Boolean {
-    return !release.title.isNullOrEmpty() || artists.isNotEmpty()
 }
