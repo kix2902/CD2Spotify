c323bdec72aa81b596bedaca65fd0d4bd7828d38
Ismael Reyes <ismael.kix2902@gmail.com>
2021-02-21 21:30:53 +0100

    Updated spotify activity with processing of album data

--- app/src/main/java/es/kix2902/cd2spotify/ui/spotify/SpotifyActivity.kt
+++ app/src/main/java/es/kix2902/cd2spotify/ui/spotify/SpotifyActivity.kt
@@ -2,9 +2,11 @@ package es.kix2902.cd2spotify.ui.spotify
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import com.squareup.picasso.Picasso
+import es.kix2902.cd2spotify.R
 import es.kix2902.cd2spotify.databinding.ActivitySpotifyBinding
 import es.kix2902.cd2spotify.ui.error.ErrorActivity
 
@@ -26,11 +28,30 @@ class SpotifyActivity : AppCompatActivity() {
         val barcode = intent.getStringExtra(EXTRA_BARCODE)!!
         viewModel.findBarcode(barcode)
 
-        viewModel.error.observe(this, Observer {
+        viewModel.error.observe(this, {
             val intent = Intent(this, ErrorActivity::class.java)
             intent.putExtra(ErrorActivity.EXTRA_ERROR, it)
             startActivity(intent)
             finish()
+        })
+        viewModel.album.observe(this, { album ->
+            if (album == null) {
+                binding.loading.visibility = View.VISIBLE
+                binding.albumInfo.visibility = View.GONE
+
+            } else {
+                binding.loading.visibility = View.GONE
+                binding.albumInfo.visibility = View.VISIBLE
+
+                Picasso.get().load(album.image).fit().centerInside().into(binding.albumImage)
+                binding.albumTitle.text = album.title
+                binding.albumArtist.text = album.artist.map { it.name }.joinToString()
+                if (album.favorite) {
+                    binding.albumFavorite.setImageResource(R.drawable.ic_favorite_filled)
+                } else {
+                    binding.albumFavorite.setImageResource(R.drawable.ic_favorite_outline)
+                }
+            }
         })
     }
 }
